6190759d4e94e70b6781ac3ac074a6558089a92b
NOUSINFO\shyamd <shyamd@nousinfo.com>
2024-10-03 17:27:49 +0530

    added missing file  name

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -12,6 +12,7 @@ const prodConfig = {
   plugins: [
     new ModuleFederationPlugin({
       name: "marketing",
+      filename: 'remoteEntry.js',
       remotes: {
         "./MarketingApp": "./src/bootstrap",
       },
